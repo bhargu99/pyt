9e76f31d79d5a8e161271d8df2b7b618bf61cbd4
bhargu99 <nalluri.bhargavi@gmail.com>
2017-09-07 18:07:58 +0530

    changes

---
+++ oopsss.py
@@ -0,0 +1,17 @@
+class test:
+	def __init__(self):
+		self.inp=""
+		self.sum1=0
+	def get_input(self):
+		print("inside")
+		inp=input("enter anystring")
+		print(inp)
+
+	def print_input(self):
+		#print(inp)
+		print(sum1)
+
+t=test()
+print("after object")
+t.get_input()
+t.print_input()
